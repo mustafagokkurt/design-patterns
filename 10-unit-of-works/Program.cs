2614024e4f88c9c79f5dcc82f39973281f526b1d
IS97163 <Mustafa.Gokkurt@softtech.isbank.com.tr>
2022-04-22 21:46:04 +0300

    unit of work

---
+++ 10-unit-of-works/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10_unit_of_works
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
