d9c5cc54a8e23f87483ac928170e8be9c912928e
IS97163 <Mustafa.Gokkurt@softtech.isbank.com.tr>
2022-04-25 09:55:58 +0300

    front controller

---
+++ 13-front-controller/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _13_front_controller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
